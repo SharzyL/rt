4d54c927145ea383589c19ccb622dc5594af45f5
SharzyL <me@sharzy.in>
2021-10-11 10:11:08 +0800

    init: base code from weblearn

---
+++ include/material.hpp
@@ -0,0 +1,41 @@
+#ifndef MATERIAL_H
+#define MATERIAL_H
+
+#include <cassert>
+#include <vecmath.h>
+
+#include "ray.hpp"
+#include "hit.hpp"
+#include <iostream>
+
+// TODO: Implement Shade function that computes Phong introduced in class.
+class Material {
+public:
+
+    explicit Material(const Vector3f &d_color, const Vector3f &s_color = Vector3f::ZERO, float s = 0) :
+            diffuseColor(d_color), specularColor(s_color), shininess(s) {
+
+    }
+
+    virtual ~Material() = default;
+
+    virtual Vector3f getDiffuseColor() const {
+        return diffuseColor;
+    }
+
+
+    Vector3f Shade(const Ray &ray, const Hit &hit,
+                   const Vector3f &dirToLight, const Vector3f &lightColor) {
+        Vector3f shaded = Vector3f::ZERO;
+        // TODO
+        return shaded;
+    }
+
+protected:
+    Vector3f diffuseColor;
+    Vector3f specularColor;
+    float shininess;
+};
+
+
+#endif // MATERIAL_H
