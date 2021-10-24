1855a092b88132205929d7ab48558997fbcf56a4
SharzyL <me@sharzy.in>
2021-10-24 16:49:30 +0800

    fix size issue

--- include/camera.hpp
+++ include/camera.hpp
@@ -11,8 +11,9 @@ public:
     Camera(const Vector3f &center, const Vector3f &direction, const Vector3f &up, int imgW, int imgH) {
         this->center = center;
         this->direction = direction.normalized();
-        this->horizontal = Vector3f::cross(this->direction, up);
+        this->horizontal = Vector3f::cross(this->direction, up).normalized();
         this->up = Vector3f::cross(this->horizontal, this->direction);
+        LOG(ERROR) << fmt::format("direction {}, up {}, horizontal {}", this->direction.length(), this->up.length(), this->horizontal.length());
         this->width = imgW;
         this->height = imgH;
     }
@@ -38,25 +39,22 @@ protected:
 class PerspectiveCamera : public Camera {
 
 public:
-    PerspectiveCamera(const Vector3f &center, const Vector3f &direction, const Vector3f &up, int imgW, int imgH,
+    PerspectiveCamera(const Vector3f &center, const Vector3f &_direction, const Vector3f &_up, int imgW, int imgH,
                       float angle)
-        : Camera(center, direction, up, imgW, imgH) {
+        : Camera(center, _direction, _up, imgW, imgH) {
         // angle is in radian.
-        auto w = (float) imgW;
-        auto dir = direction.normalized();
-        right = Vector3f::cross(dir, up);
-        float distToCanvas = w / 2 / std::tan(angle / 2);
-        Vector3f canvasCenter = center + dir * distToCanvas;
-        canvasOrigin = canvasCenter - right * (w / 2) - up * (w / 2);
+        auto w = (float) imgW, h = (float) imgH;
+        float distToCanvas = h / 2 / std::tan(angle / 2);
+        Vector3f canvasCenter = center + direction * distToCanvas;
+        canvasOrigin = canvasCenter - horizontal * (w / 2) - up * (w / 2);
     }
 
     Ray generateRay(const Vector2f &point) override {
-        Vector3f pointOnCanvas = canvasOrigin + right * point.x() + up * point.y();
+        Vector3f pointOnCanvas = canvasOrigin + horizontal * point.x() + up * point.y();
         return {center, pointOnCanvas - center};
     }
 
 protected:
-    Vector3f right;
     Vector3f canvasOrigin;
 };
 
