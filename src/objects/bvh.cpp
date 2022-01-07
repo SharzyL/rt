8a79fe36da089517f5bd0a9ef81741c9fcd6b423
SharzyL <me@sharzy.in>
2022-01-07 16:37:28 +0800

    fix: faster bvh

--- src/objects/bvh.cpp
+++ src/objects/bvh.cpp
@@ -12,12 +12,12 @@ void BVH::Build() {
 
 void BVH::build_impl(Node *node) {
     int l = node->l_idx, r = node->r_idx;
-    if (r - l < 5) {  // leaf node
-        return;
-    }
     for (int i = l; i < r; i++) {
         auto obj = objects[i];
         node->box.FitBox(obj->GetBox());
+    }
+    if (r - l < 5) {  // leaf node
+        return;
     }
     int dim = node->box.MaxSpanAxis();
     std::sort(&objects[l], &objects[r], [dim] (Object3D *obj1, Object3D *obj2) {
@@ -41,14 +41,14 @@ bool BVH::Intersect(const Ray &r, Hit &h, float tmin) const {
 bool BVH::node_intersect(const Ray &ray, Hit &h, float fmin, Node *node) const {
     int l = node->l_idx, r = node->r_idx;
     bool result = false;
-    if (node->box.IsNull()) {  // leaf node
+    if (!node->box.IsNull() && !node->box.MayIntersect(ray, fmin, h.GetT())) return false;
+
+    if (node->l_child != nullptr) {  // non-leaf
+        result |= node_intersect(ray, h, fmin, node->l_child.get());
+        result |= node_intersect(ray, h, fmin, node->r_child.get());
+    } else {  // leaf
         for (int i = l; i < r; i++) {
             result |= objects[i]->Intersect(ray, h, fmin);
-        }
-    } else {  // non-leaf node
-        if (node->box.MayIntersect(ray, fmin, h.GetT())) {
-            result |= node_intersect(ray, h, fmin, node->l_child.get());
-            result |= node_intersect(ray, h, fmin, node->r_child.get());
         }
     }
     return result;
